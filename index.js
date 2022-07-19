de285b0736513949047b83f9d3d7c0b0a76b6ceb
hmanjun <harshith.hm03@gmail.com>
2022-07-18 18:26:00 -0700

    Add required questions into an array

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require('inquirer');
+const genMarkdown = require("./utils/generateMarkdown")
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projName",
+        message: "What is the name of the project?"
+    },
+    {
+        type: "input",
+        name: "projDescr",
+        message: "Enter a brief description of the project:"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Enter instructions for installing the project:",
+        default: "(git clone ... npm install)"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter instructions for using your product: "
+    },
+    {
+        type: "input",
+        name: "credit",
+        message: "Enter credits for sources used on the project and other calobrators: "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose which license the project will use: ",
+        choices: ["MIT","GPLv2","Apache", "Unlicense"]
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter tests for the project, and how to run them: "
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter the recomended way for other to contribute, if any: "
+    },
+    {
+        type:"input",
+        name: "gUserName",
+        message: "What is you Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(err) => {
+        err ? console.log("An error occured, file was not created.") : console.log("README file was successfully created!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then()
+}
 
 // Function call to initialize app
 init();
