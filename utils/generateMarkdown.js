6ed2545cbbee15e15d3893182ed3ea01b9345f15
hmanjun <harshith.hm03@gmail.com>
2022-07-18 18:50:06 -0700

    Add generateLicenseSection function to formulate license section based on user selection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,17 +18,22 @@ function renderLicenseLink(license) {
   if(license === "Unlicense" || license === ""){
     return ""
   } else if(license === "Apache"){
-    return `https://choosealicense.com/licenses/apache-2.0/`
+    return `[Apache](https://choosealicense.com/licenses/apache-2.0/)`
   } else if(license === "MIT"){
-    return `https://opensource.org/licenses/MIT`
+    return `[MIT](https://opensource.org/licenses/MIT)`
   } else if(license === "GPLv2"){
-    return `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`
+    return `[GPLv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License
+  ${renderLicenseBadge(license)}
+  Licensed under the ${renderLicenseLink(license)} license
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
