93c6f2b971d0dac430ed711c38468f534b1652b1
hmanjun <harshith.hm03@gmail.com>
2022-07-18 18:28:32 -0700

    Add init function in index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer
         .prompt(questions)
-        .then()
+        .then(writeToFile("READEME.md",genMarkdown(response)))
 }
 
 // Function call to initialize app
